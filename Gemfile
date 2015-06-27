6e004774d8fa0cf29c39eb69abb27484714819f1
Varun Lalan <varun.lalan@kiprosh.com>
2015-06-27 17:25:07 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://production.s3.rubygems.org'
+
+gem 'rails', '4.2.2'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
+
+gem 'coffee-rails', '~> 4.1.0'
+gem "font-awesome-rails"
+gem 'foundation-rails'
+gem 'jquery-rails'
+gem 'pg'
+gem 'pickadate-rails'
+gem 'react-rails'
+gem 'sass-rails', '~> 5.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'uglifier', '>= 1.3.0'
---
+++ app/assets/javascripts/components/birthday_list.js
@@ -0,0 +1,48 @@
+var BirthdayList = React.createClass({displayName: "BirthdayList",
+  render: function() {
+    var createItem = function(arr) {
+      name = arr[0]
+      birthday = arr[1]
+      return React.createElement("li", null, name + ', ' + birthday);
+    };
+    return React.createElement("ul", null, this.props.items.map(createItem));
+  }
+});
+
+var BirthdayApp = React.createClass({displayName: "BirthdayApp",
+  getInitialState: function() {
+    return {items: [], name: '', birthday: ''};
+  },
+
+  onChangeName: function(e) {
+    this.setState({name: e.target.value});
+  },
+
+  onChangeBirthday: function(e) {
+    this.setState({birthday: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.state.items.push([this.state.name, this.state.birthday]);
+    var nextItems = this.state.items
+    this.setState({items: nextItems, name: '', birthday: ''});
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", null,
+        React.createElement("h3", null, "Birthday List"),
+        React.createElement(BirthdayList, {items: this.state.items}),
+        React.createElement("form", {onSubmit: this.handleSubmit},
+          React.createElement("h1", null, "Add Birthday"),
+          React.createElement("input", {onChange: this.onChangeName, value: this.state.name, placeholder: "Add Name"}),
+          React.createElement("br"),
+          React.createElement("input", {onChange: this.onChangeBirthday, value: this.state.birthday, placeholder: "dd/mm/yyyy"}),
+          React.createElement("br"),
+          React.createElement("button", null, 'Create Birthday')
+        )
+      )
+    );
+  }
+});
